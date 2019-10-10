fd33edcd23e9c16d5c44d5102f432b8702bd88cb
broregard <broregard@users.noreply.github.com>
2019-10-10 15:37:40 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+app.get(("/api/timestamp/:date?"), (req,res) => getTheTime(req, res, req.params.dateString));
+
+const getTheTime = (req, res, dateString) => {
+  const date = dateString ? !isNaN(dateString) ? new Date(parseInt(dateString)) : new Date(dateString) : new Date();
+  
+  if (date && !isNaN(date.valueOf())) res.json({unix: date.getTime(), utc: date.toUTCString()});
+  else res.json({error: "Invalid Date"});
+}
+
+const listener = app.listen(process.env.PORT, function() {
+  console.log("Listening on port " + listener.address().port);
+});
