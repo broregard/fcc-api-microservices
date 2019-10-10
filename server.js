1522d4288b180135a6827b58b8800f805bb3bb2e
broregard <broregard@users.noreply.github.com>
2019-10-10 16:55:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
-const express = require("express");
+onst express = require("express");
 const app = express();
 
 app.use(express.static("public"));
-app.get(("/api/timestamp/:date?"), (req,res) => getTheTime(req, res, req.params.dateString));
-app.get("*", function(request, response) {
-  response.sendFile(__dirname + "/views/index.html");
+app.get(("/api/timestamp/:date?"), (req, res) => getTimestamp(req, res));
+app.get("/api/whoami", (req, res) => getWhoAmI(req,res));
+
+app.get("*", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-const getTheTime = (req, res, dateString) => {
+
+const getTimestamp = (req, res, dateString = req.params.date) => {
   const date = dateString ? !isNaN(dateString) ? new Date(parseInt(dateString)) : new Date(dateString) : new Date();
   
   if (date && !isNaN(date.valueOf())) res.json({unix: date.getTime(), utc: date.toUTCString()});
   else res.json({error: "Invalid Date"});
 }
 
+const getWhoAmI =
+      (req, res, ipaddress = req.ip, language = req.headers["accept-language"], software = req.headers["user-agent"]) =>
+res.json({ipaddress,language,software});
+
+
 const listener = app.listen(process.env.PORT, function() {
   console.log("Listening on port " + listener.address().port);
 });
+
